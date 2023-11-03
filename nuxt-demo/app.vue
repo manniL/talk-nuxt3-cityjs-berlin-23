bced6d215ccf8c5f8f5a46cfd8e1266dc13f7d94
Alexander Lichter <github@lichter.io>
2023-11-03 12:19:38 +0100

    feat: add livecoding

--- nuxt-demo/app.vue
+++ nuxt-demo/app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const { data } = await useFetch('/api/conf')
+</script>
+
 <template>
   <div class="container">
-    Let's go!
+    <h1>Let's go!</h1>
+    {{ data }}
   </div>
 </template>
